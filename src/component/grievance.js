51b3f7e2924a3f1450c786a658098311bd6e84c2
SubhasishBehera <subhasishbehera333@gmail.com>
2020-06-18 06:18:37 -0800

    Files

---
+++ src/component/grievance.js
@@ -0,0 +1,180 @@
+import React, { useState, useEffect} from 'react'
+
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { withRouter } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import UserWrapper from './UserWrapper';
+import Paper from "@material-ui/core/Paper";
+import Grid from '@material-ui/core/Grid';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import axios from "axios"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        minHeight:5,
+    },
+    container: {
+        margin: theme.spacing(0),
+        display: "flex",
+        flexWrap: "wrap",
+    },
+    top:{
+        fontSize: "2em",
+        fontWeight: "bold",
+        
+    },
+    leftSide: {
+        flex: 1,
+        height: "100%",
+        margin: theme.spacing(1),
+        padding: theme.spacing(0),
+    },
+    rightSide: {
+        flex: 1,
+        height: "100%",
+        margin: theme.spacing(1),
+        padding: theme.spacing(1),
+    },
+    paper: {
+        height: "100%",
+        margin: theme.spacing(1),
+        padding: theme.spacing(2),
+    },
+    grid: {
+        margin: theme.spacing(1),
+       
+
+    },
+   
+    textField: {
+        height: 'auto',
+        
+      },
+    
+    h2: {
+        color: 'black',
+        fontSize: "0.7em",
+        margin: theme.spacing(1, 2),
+    },
+}));
+const Grievance = props => {
+
+
+    
+    const classes = useStyles();
+    function handleSubmit() {
+        setDisabled(true);
+    };
+    let currentUser = JSON.parse(localStorage.getItem("user"));
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': currentUser.token ? `Bearer${currentUser.token}` : ''
+    }
+
+   
+
+    const [disabled, setDisabled] = useState(true);
+    const [grievance, updateGrievance] = useState([]);
+
+    console.log("Grievance :",grievance[0] ? grievance[0].Name : '');
+    return (
+        <Formik
+            initialValues={
+                {
+                    name: grievance[0] ? (grievance[0]).Name : '' ,
+                    detail: "Write details of you Grievance ....",                    
+                    occupation: "Student",
+                    age: "22",
+                    marital: "Unmarried"
+                }
+            }
+            onSubmit={handleSubmit}
+          
+        >
+            {props => {
+                const {
+                    values,
+                    
+                    handleSubmit
+                } = props;
+                return (
+                    <UserWrapper>
+                        <div className={classes.root}>
+                        
+                      
+                                            
+                            <div className={classes.container} alignItems="center" justify="center">
+                            
+                                <div className={classes.leftSide} alignItems="center" justify="center">
+                                <div className={classes.top}>
+                                <Grid container spacing={2} alignItems="center" justify="center">
+                                Grievance Portal
+                            </Grid>
+                            </div>
+
+
+                               <grid  item xs={6}>
+                                    <form onSubmit={handleSubmit}>
+                                    <Paper className={classes.paper}>
+                                        <Paper className={classes.paper}>
+                                            
+                                        <Grid item xs={12} >
+                                        <TextField placeholder="Title:" variant="outlined"  fullWidth />
+                                        </Grid>
+                                        </Paper>
+                                        
+                                        <div className={classes.text}>
+                                        <Paper className={classes.paper}>
+
+                                        <Grid item xs={12} >
+                                        <TextField
+                                        id="outlined-multiline-static"
+                                        
+                                        multiline
+                                        rows={5}
+                                    
+                                        variant="outlined"
+                                        
+                                        placeholder="Enter you Grievance in detail...." variant="outlined"  fullWidth />
+                                        </Grid>
+                                        </Paper>
+                                        </div>
+                                          
+                                        <input
+                                        accept="image/*"
+                                        className={classes.input}
+                                        style={{ color:"red"}}
+                                        id="raised-button-file"
+                                        multiple
+                                        type="file"
+                                      />
+                                      <label htmlFor="raised-button-file" >
+                                        <Button variant="contained" color="default" component="span">
+                                        <CameraAltIcon color="action" /> Upload
+                                        </Button>
+                                      </label>
+                                             <Grid container spacing={4} alignItems="center" justify="flex-end">
+                                                <Grid item ><Button variant="contained" color="primary" onClick={handleSubmit}><SearchIcon color="action" />Submit</Button></Grid>
+                                            </Grid>
+                                 </Paper>
+                                            
+                                    </form>
+                                    </grid>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </UserWrapper>
+                )
+            }}
+        </Formik>
+    )
+ 
+}
+
+export default withRouter(Grievance);
